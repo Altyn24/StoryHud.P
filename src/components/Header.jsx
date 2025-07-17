1d7e2769b9010dd9a59abaed64b4c621fbde582a
Altyn <altynbekgrozy@gmail.com>
2025-07-16 17:00:35 -1200

    Обнавлённый хедер

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
 
   return (
     <>
-      <div className="p-3 w-full max-w-screen-xl border-b-2 border-[#CC2E2E] mb-3 mx-auto">
+      <div className="p-3 w-full max-w-screen-xl shadow-md mb-3 mx-auto">
         <header className="flex justify-between items-center">
           <div className="flex items-center gap-5">
             <Link to="/" className="text-2xl font-bold">
@@ -48,13 +48,30 @@ function Header() {
               <input
                 type="text"
                 id="search"
-                className="p-2 border-b-1 border-gray-300"
-                placeholder="Введите название"
+                className="p-2 outline-none"
+                placeholder="Поиск"
               />
             </div>
           </div>
-          <div className="flex items-center gap-5">
-            <Link to="/create-post" className="text-gray-500">
+          
+          <div className="flex items-center gap-8">
+            <Link to="/create-post" className="text-gray-500 flex items-center">
+              <label>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+              </label>
               Написать историю
             </Link>
             {isAuth && user ? (
@@ -114,16 +131,16 @@ function Header() {
           </div>
         )}
 
-        <nav className="flex flex-col gap-4 ">
-          <Link to="/" onClick={closeSidebar} className="hover:underline">
-            Главная
-          </Link>
+        <nav className="flex flex-col gap-4 border-b-1 p-2">
           <Link
             to="/profile"
             onClick={closeSidebar}
             className="hover:underline"
           >
             Профиль
+          </Link>
+          <Link to="/" onClick={closeSidebar} className="hover:underline">
+            Главная
           </Link>
           <button
             onClick={handleLogout}
@@ -138,6 +155,5 @@ function Header() {
 }
 
 export default Header;
-
 
 // "https://i.pravatar.cc/100"
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 function Home() {
+  // const [content, setContent] = useState("");
+  const content = ["Новые", "Популярные", "Обновление"];
+
   return (
-    <main className="container mx-auto p-4 border-1 border-gray-300 rounded-xl max-w-screen justify-items-center">
-      <p className="text-lg">
-        Это место, где вы можете делиться своими историями и вдохновлять других.
-      </p>
-      
+    <main className="container mx-auto p-4 rounded-xl max-w-screen">
+      <div className="justify-items-start max-w-screen">
+        <ul className="flex gap-5 border-b-1 border-gray-300 p-2 w-full">
+          {content.map((item) => (
+            <li className="cursor-pointer hover:text-[#3E13AF]">{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="justify-items-center">
+        <p className="text-lg">
+          Это место, где вы можете делиться своими историями и вдохновлять
+          других.
+        </p>
+      </div>
     </main>
   );
 }
