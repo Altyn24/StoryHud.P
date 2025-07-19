7316171d5f29188433f255fb8f7a0370a29902c6
Altyn <altynbekgrozy@gmail.com>
2025-07-19 11:34:00 -1200

    лента

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchStories } from "../features/auth/storiesSlice";
 import StoryCard from "./StoryCards";
+import { Flex, Spin } from "antd";
 
 function Home() {
   const dispatch = useDispatch();
@@ -17,7 +18,14 @@ function Home() {
     <main className="max-w-2xl mx-auto p-4">
       <h2 className="text-2xl font-bold mb-6">Сегодяшная лента</h2>
 
-      {status === "loading" && <p>Загрузка...</p>}
+      {status === "loading" && (
+        <div className="justify-items-center">
+          {" "}
+          <Flex align="center" gap="middle">
+            <Spin size="large" />
+          </Flex>
+        </div>
+      )}
       {status === "failed" && <p>Ошибка при загрузке историй</p>}
 
       {items.map((story) => (
